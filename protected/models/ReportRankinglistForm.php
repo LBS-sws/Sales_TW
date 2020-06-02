aea1e37b3f6686c05a0ba8ec49675d6b4755527c
anxiaxia <1286571499@qq.com>
2020-06-02 11:28:39 +0800

    销售分类汇总修改

--- protected/models/ReportRankinglistForm.php
+++ protected/models/ReportRankinglistForm.php
@@ -63,10 +63,11 @@ class ReportRankinglistForm extends CReportForm
         $suffix = Yii::app()->params['envSuffix'];
         $models = array();
        // $time= date('Y-m-d', strtotime(date('Y-m-01') ));
+        $obj_where = $this->getDealString("a.visit_obj");
         $sql = "select a.city, a.username, sum(convert(b.field_value, decimal(12,2))) as money
 				from sal_visit a force index (idx_visit_02), sal_visit_info b
 				where a.id=b.visit_id and b.field_id in ('svc_A7','svc_B6','svc_C7','svc_D6','svc_E7') 
-				and a.visit_dt >= '$start'and a.visit_dt <= '$end' and  a.visit_obj like '%10%'
+				and a.visit_dt >= '$start'and a.visit_dt <= '$end' and  ($obj_where)
 				group by a.city, a.username
 			";
         $records = Yii::app()->db->createCommand($sql)->queryAll();
@@ -105,7 +106,7 @@ class ReportRankinglistForm extends CReportForm
        // $time= date('Y-m-d', strtotime(date('Y-m-01') ));
         foreach ($cities as $code=>$name) {
             $sum_arr = array();
-            if (strpos("/'CS'/'H-N'/'HK'/'TC'/'ZS1'/'TP'/'TY'/'KS'/'TN'/'XM'/'KH'/'ZY'/'MO'/'RN'/'MY'/","'".$code."'")===false) {
+            if (strpos("/'CN'/'HK'/","'".$code."'")===false) {
                 $sql = "select a.name as city_name, b.name as region_name 
 						from security$suffix.sec_city a
 						left outer join security$suffix.sec_city b on a.region=b.code
@@ -120,7 +121,8 @@ class ReportRankinglistForm extends CReportForm
                 $peoples=count($people);
                 if(!empty($people)){
                     //总单数
-                    $sql2="select id from sal_visit where city='$code' and  visit_obj like '%10%' and visit_dt >='".$start."'and visit_dt <='".$end."'";
+                    $obj_where = $this->getDealString("visit_obj");
+                    $sql2="select id from sal_visit where city='$code' and  ($obj_where) and visit_dt >='".$start."'and visit_dt <='".$end."'";
                     $sum = Yii::app()->db->createCommand($sql2)->queryAll();
                     foreach ($sum as $id){
                         $sqlid="select count(visit_id) as sum from  sal_visit_info where field_id in ('svc_A7','svc_B6','svc_C7','svc_D6','svc_E7') and field_value>'0' and visit_id='".$id['id']."'";
@@ -155,7 +157,7 @@ class ReportRankinglistForm extends CReportForm
         $cities = General::getCityListWithNoDescendant();
      //   $time= date('Y-m-d', strtotime(date('Y-m-01') ));
         foreach ($cities as $code=>$name) {
-            if (strpos("/'CS'/'H-N'/'HK'/'TC'/'ZS1'/'TP'/'TY'/'KS'/'TN'/'XM'/'KH'/'ZY'/'MO'/'RN'/'MY'/","'".$code."'")===false) {
+            if (strpos("/'CS'/'HK'/","'".$code."'")===false) {
                 $sql = "select a.name as city_name, b.name as region_name 
 						from security$suffix.sec_city a
 						left outer join security$suffix.sec_city b on a.region=b.code
@@ -169,7 +171,8 @@ class ReportRankinglistForm extends CReportForm
                 $people = Yii::app()->db->createCommand($sql1)->queryAll();
                 $peoples=count($people);
                 //总单数
-                $sql2="select id from sal_visit where city='$code' and  visit_obj like '%10%' and visit_dt >='".$start."'and visit_dt <='".$end."'";
+                $obj_where = $this->getDealString("visit_obj");
+                $sql2="select id from sal_visit where city='$code' and  ($obj_where) and visit_dt >='".$start."'and visit_dt <='".$end."'";
                 $sum = Yii::app()->db->createCommand($sql2)->queryAll();
                 $sums=count($sum);
                 //人均签单数
@@ -204,4 +207,13 @@ class ReportRankinglistForm extends CReportForm
         return $result;
     }
 
+    private function getDealString($field) {
+        $rtn = '';
+        $sql = "select id from sal_visit_obj where rpt_type='DEAL'";
+        $rows = Yii::app()->db->createCommand($sql)->queryAll();
+        foreach ($rows as $row) {
+            $rtn .= ($rtn=='' ? '' : ' or ').$field." like '%\"".$row['id']."\"%'";
+        }
+        return ($rtn=='' ? "$field='0'" : $rtn);
+    }
 }
--- protected/models/ReportVisitForm.php
+++ protected/models/ReportVisitForm.php
@@ -1805,10 +1805,11 @@ class ReportVisitForm extends CReportForm
         foreach ($model['sale'] as $code=>$peoples){
             $sum_arr=array();
             $people=array();
+            $obj_where = $this->getDealString("a.visit_obj");
             $sql = "select a.city, a.username, sum(convert(b.field_value, decimal(12,2))) as money 
 				from sal_visit a force index (idx_visit_02), sal_visit_info b   
 				where a.id=b.visit_id and b.field_id in ('svc_A7','svc_B6','svc_C7','svc_D6','svc_E7') 
-				and a.visit_dt >= '$start_dt'and a.visit_dt <= '$end_dt' and  a.visit_obj like '%10%' and a.username ='$peoples' 
+				and a.visit_dt >= '$start_dt'and a.visit_dt <= '$end_dt' and  ($obj_where) and a.username ='$peoples' 
 				group by a.city, a.username 
 			";
             $records = Yii::app()->db->createCommand($sql)->queryAll();
@@ -1822,7 +1823,8 @@ class ReportVisitForm extends CReportForm
             $sqls="select a.name as cityname ,d.name as names from security$suffix.sec_city a	,hr$suffix.hr_binding b	 ,security$suffix.sec_user  c ,hr$suffix.hr_employee d 
                 where c.username='$peoples' and b.user_id='".$peoples."' and b.employee_id=d.id and c.city=a.code";
             $cname = Yii::app()->db->createCommand($sqls)->queryRow();
-            $sql1="select id  from sal_visit where username='".$peoples."'  and  visit_dt >= '$start_dt'and visit_dt <= '$end_dt' and visit_obj like '%10%'";
+            $obj_where = $this->getDealString("visit_obj");
+            $sql1="select id  from sal_visit where username='".$peoples."'  and  visit_dt >= '$start_dt'and visit_dt <= '$end_dt' and ($obj_where)";
             $arr = Yii::app()->db->createCommand($sql1)->queryAll();
             foreach ($arr as $id){
                 $sqlid="select count(visit_id) as sum from  sal_visit_info where field_id in ('svc_A7','svc_B6','svc_C7','svc_D6','svc_E7','svc_F4','svc_G3') and field_value>'0' and visit_id='".$id['id']."'";
@@ -1967,5 +1969,16 @@ class ReportVisitForm extends CReportForm
         header('Content-Disposition:attachment;filename="'.$str.'"');
         header("Content-Transfer-Encoding:binary");
         echo $output;
+    }
+
+
+    private function getDealString($field) {
+        $rtn = '';
+        $sql = "select id from sal_visit_obj where rpt_type='DEAL'";
+        $rows = Yii::app()->db->createCommand($sql)->queryAll();
+        foreach ($rows as $row) {
+            $rtn .= ($rtn=='' ? '' : ' or ').$field." like '%\"".$row['id']."\"%'";
+        }
+        return ($rtn=='' ? "$field='0'" : $rtn);
     }
 }
