4281a5cb48640472f1aea96f592cace08ddfe6cc
anxiaxia <1286571499@qq.com>
2021-01-18 10:53:48 +0800

    测试查找

--- protected/controllers/FivestepController.php
+++ protected/controllers/FivestepController.php
@@ -98,19 +98,64 @@ class FivestepController extends Controller
 
 	public function actionIndex($pageNum=0) 
 	{
-		$model = new FivestepList;
-		if (isset($_POST['FivestepList'])) {
-			$model->attributes = $_POST['FivestepList'];
-		} else {
-			$session = Yii::app()->session;
-			if (isset($session[$model->criteriaName()]) && !empty($session[$model->criteriaName()])) {
-				$criteria = $session[$model->criteriaName()];
-				$model->setCriteria($criteria);
-			}
-		}
-		$model->determinePageNum($pageNum);
-		$model->retrieveDataByPage($model->pageNum);
-		$this->render('index',array('model'=>$model));
+        $suffix = Yii::app()->params['envSuffix'];
+        $firstDay = date("Y-m-d");
+        $arr['start_dt'] = date("Y-m-d", strtotime("$firstDay - 6 day"));
+        $arr['end_dt'] = $firstDay;
+        //收件人
+        $sql = "select a.username,a.email,a.city,c.name from  security$suffix.sec_user a 
+              inner join security$suffix.sec_user_access b on a.username = b.username 
+              inner join security$suffix.sec_city c on a.city = c.code 
+              where b.system_id='sal' and b.a_control like '%CN08%' and a.status='A'
+              ";
+        $records = Yii::app()->db->createCommand($sql)->queryAll();
+        foreach ($records as $Addressee) {
+            //城市
+            $model = new City();
+            $record = $model->getDescendant($Addressee['city']);
+            array_unshift($record, $Addressee['city']);
+            foreach ($record as $k) {
+                $nocity = array('CN', 'CS', 'H-N', 'HB', 'HD', 'HD1', 'HK', 'HN', 'HN1', 'HN2', 'HX', 'HXHB', 'JMS', 'MO', 'MY', 'RN', 'TY', 'XM', 'ZS1', 'ZY');
+                $sql_city = "select name from security$suffix.sec_city where code='$k'";
+                $city = Yii::app()->db->createCommand($sql_city)->queryScalar();
+                if (in_array($k, $nocity, true)) {
+                } else {
+                    //需要的销售
+                    $sql_people = "select a.name,e.username from hr$suffix.hr_employee a
+                              inner join  hr$suffix.hr_binding b on a.id=b.employee_id 
+                              inner join  security$suffix.sec_user_access c on b.user_id=c.username  
+                              inner join  security$suffix.sec_user d on c.username=d.username 
+                              inner join  sales$suffix.sal_visit e on b.user_id=e.username
+       where  c.system_id='sal' and c.a_read_write like '%HK01%' and  d.status='A' and a.city='$k' and   e.visit_dt >= '" . $arr['start_dt'] . "'and e.visit_dt <= '" . $arr['end_dt'] . "' and  a.staff_status =0";
+                    $people = Yii::app()->db->createCommand($sql_people)->queryAll();
+                    //邮件数据
+                    if (!empty($people)) {
+                        $people = array_unique($people, SORT_REGULAR);
+                        $arr['sale'] = array_column($people, 'username');
+                        $arr['sort'] = 'singular';
+                        $arr_email = ReportVisitForm::Summary($arr);
+                        print_r('<pre>'); print_r($sql_people); print_r($arr);print_r('-----');print_r($arr_email);
+                    }
+                }
+            }
+        }
+
+
+
+
+//		$model = new FivestepList;
+//		if (isset($_POST['FivestepList'])) {
+//			$model->attributes = $_POST['FivestepList'];
+//		} else {
+//			$session = Yii::app()->session;
+//			if (isset($session[$model->criteriaName()]) && !empty($session[$model->criteriaName()])) {
+//				$criteria = $session[$model->criteriaName()];
+//				$model->setCriteria($criteria);
+//			}
+//		}
+//		$model->determinePageNum($pageNum);
+//		$model->retrieveDataByPage($model->pageNum);
+//		$this->render('index',array('model'=>$model));
 	}
 
 /*
